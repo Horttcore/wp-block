b1ce8649bebad30ceef5712527a58b1edda013f1
Ralf Hortt <me@horttcore.de>
2020-05-25 09:47:52 +0000

    Apply fixes from StyleCI

--- src/Block.php
+++ src/Block.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace RalfHortt\WPBlock;
 
 use RalfHortt\ServiceContracts\ServiceContract;
@@ -12,21 +13,21 @@ abstract class Block implements ServiceContract
     protected $attributes = [];
 
     /**
-     * Register
+     * Register.
      */
     public function register(): void
     {
         register_block_type(
             $this->getName(),
             [
-                'render_callback' => [$this,'callback'],
+                'render_callback' => [$this, 'callback'],
                 'attributes'      => $this->getAttributes(),
             ]
         );
     }
 
     /**
-     * Get block name
+     * Get block name.
      */
     public function getName(): string
     {
@@ -34,29 +35,29 @@ abstract class Block implements ServiceContract
     }
 
     /**
-     * Get block attributes
+     * Get block attributes.
      */
     public function getAttributes(): array
     {
-        return apply_filters($this->getName() . '/attributes', $this->attributes);
+        return apply_filters($this->getName().'/attributes', $this->attributes);
     }
 
     /**
-     * Render callback
+     * Render callback.
      */
     public function callback(array $atts = [], string $content = ''): string
     {
         ob_start();
 
-        do_action($this->getName() . '/before', $atts, $content);
-        echo apply_filters($this->getName() . '/render', $this->render($atts, $content), $atts, $content);
-        do_action($this->getName() . '/after', $atts, $content);
+        do_action($this->getName().'/before', $atts, $content);
+        echo apply_filters($this->getName().'/render', $this->render($atts, $content), $atts, $content);
+        do_action($this->getName().'/after', $atts, $content);
 
         return ob_get_clean();
     }
 
     /**
-     * Output
+     * Output.
      */
     abstract protected function render(array $atts, string $content): void;
 }
